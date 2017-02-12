bcc5a185690949de3ae565c93dbe746ab87b583a
Ogromny <ogromny@openmailbox.org>
2017-02-12 19:10:03 +0100

    Removed unnecessary comments

--- lib/parol/database.rb
+++ lib/parol/database.rb
@@ -2,42 +2,25 @@ require 'rbnacl/libsodium'
 
 module Parol
 
+  class ParolError < StandardError; end
+  class BadPasswordLength < ParolError; end
+  class DecryptionFailed < ParolError; end # wrong password or wrong file
+
   class Database
 
-    # init instance variable and check if password is 32 of length else return 'PASSWORD_IS_NOT_32_OF_LENGTH'
     def initialize database, password
       @database = database
       @password = password
 
-      unless @password.length == 32
-        return 'PASSWORD_IS_NOT_32_OF_LENGTH'
-      end
-
       @binary_password = String.new @password, encoding: 'BINARY'
-
       @box = RbNaCl::SimpleBox.from_secret_key @binary_password
-    end
 
-    # ...
-    def remove index
-      index = Integer index
-      data = decrypt
-
-      if data.delete_at(index).nil?
-        return 'INDEX_DOES_NOT_EXIST'
-      end
-
-      encrypt data
-    rescue
-      return 'INDEX_IS_PROBABLY_NOT_AN_INDEX'
+      rescue RbNaCl::LengthError
+        raise BadPasswordLength
     end
 
     def add program, login, password, notes
       data = decrypt
-
-      if data == 'PASSWORD_IS_PROBABLY_INVALID'
-        halt data
-      end
 
       data << {
           program: program,
@@ -50,50 +33,43 @@ module Parol
     end
 
     def accounts &block
-      unless block_given?
-        return
-      end
-
       data = decrypt
 
-      if data == 'PASSWORD_IS_PROBABLY_INVALID'
-        abort data
-      end
-
-      data.each do |h|
-        yield h
+      if block_given?
+        data.each do |h|
+          yield h
+        end
+      else
+        data
       end
     end
 
     private
-      # load and decrypt the database if an exception happen 'PASSWORD_IS_PROBABLY_INVALID' is returned
-        def decrypt
-          unless File.exist? @database
-            create
-            puts 'created'
-          end
-
-          data = File.binread @database
-          Marshal.load @box.decrypt(data)
-        rescue Exception => e
-          abort "PASSWORD_IS_PROBABLY_INVALID: #{e.message}"
-        end
-
-      # encrypt the data and save it in the database
-      def encrypt data
-        File.open @database, 'wb' do |file|
-          dump = Marshal.dump data
-          file.write @box.encrypt dump
-        end
+    def decrypt
+      unless File.exist? @database
+        create
       end
 
-      # create an empty database
-      def create
-        File.open @database, 'wb' do |file|
-          dump = Marshal.dump Array.new
-          file.write @box.encrypt(dump)
-        end
+      data = File.binread @database
+      Marshal.load @box.decrypt(data)
+
+    rescue RbNaCl::CryptoError
+      raise DecryptionFailed
+    end
+
+    def encrypt data
+      File.open @database, 'wb' do |file|
+        dump = Marshal.dump data
+        file.write @box.encrypt dump
       end
+    end
+
+    def create
+      File.open @database, 'wb' do |file|
+        dump = Marshal.dump Array.new
+        file.write @box.encrypt(dump)
+      end
+    end
 
   end
 
--- parol.gemspec
+++ parol.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
     s.name        = 'parol'
-    s.version     = '3.5.0'
+    s.version     = '3.5.1'
     s.date        = '2017-02-12'
     s.summary     = 'parol'
     s.description = 'A secure password manager'
@@ -9,8 +9,5 @@ Gem::Specification.new do |s|
     s.files       = %w(lib/parol.rb lib/parol/database.rb)
     s.homepage    = 'https://github.com/Ogromny/parol/'
     s.license     = 'MIT'
-    # s.executables << 'parol'
-    # s.add_dependency 'thor', '~> 0.19', '>= 0.19.1'
     s.add_dependency 'rbnacl-libsodium', '~> 0'
-    # s.add_dependency 'orpg', '~> 0'
 end
